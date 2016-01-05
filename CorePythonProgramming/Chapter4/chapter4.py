d89aa3917eaa7c122bfac8f773482fab5c893f6f
feiatnokia <fei.gao@gmail.com>
2016-01-05 10:24:51 +0800

    chapter3

---
+++ CorePythonProgramming/Chapter4/chapter4.py
@@ -0,0 +1,18 @@
+__author__ = 'f2gao'
+
+# foolist = ['xxxa', 'a34', '3333', 'rteee', '3455']
+# print foolist[::-1]
+# foolist.reverse()
+# print(foolist)
+
+# a = [ 5, 'hat', -9.3]
+# b = [ 5, 'hat', -9.3]
+# print a is b
+# c = a
+# print a is c
+# print id(a), id(b), id(c)
+
+aString = 'abc'
+print repr(aString)
+
+print 'test'
